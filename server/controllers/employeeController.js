2ed2b7f0be37dd01a65a96bf70605cccfd62464a
DOMAA21 <clashecurry21@gmail.com>
2023-11-11 10:40:07 +0800

    employeeCRud

--- server/controllers/employeeController.js
+++ server/controllers/employeeController.js
@@ -80,6 +80,109 @@ exports.newEmployee = async (req, res, next) => {
   }
 };
 
+exports.getEmployeeDetails = async (req, res, next) => {
+  const employee = await User.findById(req.params.id);
+
+  if (!employee) {
+    return next(
+      new ErrorHandler(`Employee does not found with id: ${req.params.id}`)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+
+    employee,
+  });
+};
+
+exports.updateEmployee = async (req, res, next) => {
+  const { fname, lname, course, religion, email, password } = req.body;
+
+  try {
+    const employee = await User.findById(req.params.id);
+    
+    if (!employee) {
+      return res.status(404).json({
+        success: false,
+        message: 'User not found.',
+      });
+    }
+
+    if (email !== employee.email) {
+      const existingUser = await User.findOne({ email });
+
+      if (existingUser) {
+        return res.status(400).json({
+          success: false,
+          message: 'Email is already registered.',
+        });
+      }
+    }
+
+    const newEmployeeData = {
+      fname,
+      lname,
+      course,
+      religion,
+      role: 'Employee',
+      email,
+      password
+    };
+
+
+    if (req.file && req.file.path !== null) {
+      const image_id = employee.avatar.public_id;
+      const deleteResult = await cloudinary.uploader.destroy(image_id);
+
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        folder: "avatars",
+      });
+
+      newEmployeeData.avatar = {
+        public_id: result.public_id,
+        url: result.secure_url,
+      };
+    }
+
+    const updatedEmployee = await User.findByIdAndUpdate(req.params.id, newEmployeeData, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+
+    res.status(200).json({
+      success: true,
+      employee: updatedEmployee,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: 'An error occurred while updating the user.',
+    });
+  }
+};
+
+exports.deleteEmployee = async (req, res, next) => {
+  const employee = await User.findById(req.params.id);
+  if (!employee) {
+    return next(
+      new ErrorHandler(`Employee does not found with id: ${req.params.id}`)
+    );
+  }
+  const image_id = employee?.avatar?.public_id;
+
+  await cloudinary.v2.uploader.destroy(image_id);
+
+  await employee.deleteOne();
+
+  res.status(200).json({
+    success: true,
+  });
+
+};
+
 
 
 
--- server/routes/employee.js
+++ server/routes/employee.js
@@ -12,14 +12,20 @@ const {
 
 const {
     allEmployees,
-    newEmployee
+    newEmployee,
+    getEmployeeDetails,
+    updateEmployee,
+    deleteEmployee
 } = require("../controllers/employeeController");
 
 
 router
   .route("/admin/store/:id/employees")
   .get(isAuthenticatedUser,authorizeRoles('Admin', 'Employee'),allEmployees);
-
+   router.route('/admin/employee/:id')
+  .get(isAuthenticatedUser,authorizeRoles('Admin', 'Employee'),getEmployeeDetails)
+  .put(isAuthenticatedUser,authorizeRoles('Admin', 'Employee'),upload.single("avatar"),updateEmployee)
+  .delete(isAuthenticatedUser,authorizeRoles('Admin', 'Employee'),deleteEmployee);
 router.post("/admin/employee/new",isAuthenticatedUser,authorizeRoles('Admin', 'Employee'), upload.single("avatar"), newEmployee);
 
 
