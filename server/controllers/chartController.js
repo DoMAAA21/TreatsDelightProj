db56b157d2228d43a00a9fc896b9fdf050f8cbfa
DOMAA21 <clashecurry21@gmail.com>
2023-11-30 18:04:42 +0800

    minor changes

--- server/controllers/chartController.js
+++ server/controllers/chartController.js
@@ -35,3 +35,92 @@ exports.ordersPerMonth = async (req, res, next) => {
     }
 }
 
+exports.productsSold = async (req, res, next) => {
+    try {
+      const salesData = await Order.aggregate([
+        {
+          $unwind: '$orderItems',
+        },
+        {
+          $group: {
+            _id: '$orderItems.name',
+            totalProductsSold: { $sum: '$orderItems.quantity' },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            label: '$_id',
+            value: '$totalProductsSold',
+          },
+        },
+        {
+          $sort: {
+            value: -1,
+          },
+        },
+        {
+          $facet: {
+            topProducts: [
+              { $limit: 5 }, // Take the top 5 products
+            ],
+            others: [
+              { $skip: 5 }, // Skip the top 5 products
+            ],
+          },
+        },
+        {
+          $project: {
+            data: {
+              $concatArrays: ['$topProducts', [{ label: 'Others', value: { $sum: '$others.value' } }]],
+            },
+          },
+        },
+      ]);
+  
+      res.json(salesData[0].data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+exports.salesPerMonth = async (req, res, next) => {
+    try {
+        const salesData = await Order.aggregate([
+            {
+                $unwind: '$orderItems',
+            },
+            {
+                $group: {
+                    _id: {
+                        month: { $month: '$createdAt' },
+                        order: '$_id',
+                    },
+                    totalSales: { $sum: { $multiply: ['$orderItems.price', '$orderItems.quantity'] } },
+                },
+            },
+            {
+                $group: {
+                    _id: '$_id.month',
+                    totalSales: { $sum: '$totalSales' },
+                },
+            },
+            {
+                $sort: {
+                    '_id': 1,
+                },
+            },
+        ]);
+
+        res.json(salesData);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+  
+  
+
+
--- server/routes/chart.js
+++ server/routes/chart.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router();
 const { ordersPerMonth,
+    productsSold,
+    salesPerMonth
 } = require('../controllers/chartController')
 
 router.route('/chart/orders-per-month').get(ordersPerMonth);
+router.route('/chart/products-sold').get(productsSold);
+router.route('/chart/sales-per-month').get(salesPerMonth);
 
 module.exports = router;
